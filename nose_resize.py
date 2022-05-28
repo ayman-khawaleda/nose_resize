164f15b041340edfe6790bbac6f8b5d9cb56cf13
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-05-28 19:12:31 +0300

    Remaping The IndexMap & Image

--- nose_resize.py
+++ nose_resize.py
@@ -4,6 +4,7 @@ import numpy as np
 
 import matplotlib
 import matplotlib.pyplot as plt
+
 matplotlib.use("GTK4Agg")
 
 
@@ -14,8 +15,9 @@ class NoseResizeTool(FaceTool):
         self.image = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
         self.orig = self.image.copy()
 
-    def apply(self, size=0.9, *args, **kwargs):
+    def apply(self, size=0.9,k=5, *args, **kwargs):
         """size: Scaling Factor of Nose
+        \nk: Is For The Kernal of Gaussian Blur
         \nkwargs:
             \nFile: Path For The Image To Be Modifed.
             \nRadius: The Region Around The Eye Where All Processing Are Done.
@@ -41,17 +43,19 @@ class NoseResizeTool(FaceTool):
             face_upper[1] : face_lower[1], face_upper[0] : face_lower[0], :
         ].copy()
         self.image[nose_p[1], nose_p[0], :] = (255, 0, 0)
-        face2 = self.image[
-            face_upper[1] : face_lower[1], face_upper[0] : face_lower[0], :
-        ].copy()
+
         h, w = face.shape[0], face.shape[1]
-        self.radius = 170 if h > 800 or w > 800 else 45
-        self.nose_p = self.__detect_nose_tip(face2)
+        self.radius = 160 if h > 800 or w > 800 else 50
+        self.nose_p = self.__detect_nose_tip(face_upper, face_lower)
         self.__create_index_map(h, w)
         self.__edit_nose_area()
-        self.__smothe_border()
+        self.__smothe_border(k=k)
+        self.__remaping(face, face_upper, face_lower)
 
-    def __detect_nose_tip(self, subface):
+    def __detect_nose_tip(self, face_upper, face_lower):
+        subface = self.image[
+            face_upper[1] : face_lower[1], face_upper[0] : face_lower[0], :
+        ].copy()
         indx = np.where(subface[:, :, 0] == 255)
         p = 0
         for x, y in zip(indx[0], indx[1]):
@@ -90,11 +94,21 @@ class NoseResizeTool(FaceTool):
                         self.nose_p[0] - (-j / self.radius) ** self.power * self.radius
                     )
 
-    def __smothe_border(self,k=(5,5),xspace=10,yspace=10,sigmax=0):
-        y,x = self.nose_p
+    def __smothe_border(self, k=5, xspace=10, yspace=10, sigmax=0):
+        y, x = self.nose_p
         r = self.radius
-        lU = [y-r-yspace,x-r-xspace] #Left Upper
-        rL = [y+r+yspace,x+r+xspace] #Right Lower
-        self.nose_map_x[lU[1]:rL[1],lU[0]:rL[0]] = cv2.GaussianBlur(self.nose_map_x[lU[1]:rL[1],lU[0]:rL[0]].copy(),k,sigmax)
-        self.nose_map_y[lU[1]:rL[1],lU[0]:rL[0]] = cv2.GaussianBlur(self.nose_map_y[lU[1]:rL[1],lU[0]:rL[0]].copy(),k,sigmax)
-        
+        lU = [y - r - yspace, x - r - xspace]  # Left Upper
+        rL = [y + r + yspace, x + r + xspace]  # Right Lower
+        self.nose_map_x[lU[1] : rL[1], lU[0] : rL[0]] = cv2.GaussianBlur(
+            self.nose_map_x[lU[1] : rL[1], lU[0] : rL[0]].copy(), (k,k), sigmax
+        )
+        self.nose_map_y[lU[1] : rL[1], lU[0] : rL[0]] = cv2.GaussianBlur(
+            self.nose_map_y[lU[1] : rL[1], lU[0] : rL[0]].copy(), (k,k), sigmax
+        )
+
+    def __remaping(self, face, face_upper, face_lower):
+        warped = cv2.remap(face, self.nose_map_x, self.nose_map_y, cv2.INTER_CUBIC)
+        self.image[
+            face_upper[1] : face_lower[1], face_upper[0] : face_lower[0], :
+        ] = warped
+
